94ebc741948915b2f2a5b974661fdf36cd4b62f7
DESKTOP-65SAUST\Alumno <javi919290@gmail.com>
2025-01-22 15:38:35 +0100

    ainaracara_cruz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
-    
-    console.log("Online!");
-})
+
+    let $coin = $("#coin");
+
+    $("#coin").on("click", function() {
+        let randomNum = Math.random();
+        $coin.removeClass();
+        
+     setTimeout(function() {
+         if (randomNum <= 0.5) {
+            console.log("Es cara");
+            $coin.addClass ("isHeads")
+         }
+
+
+         else {
+            console.log("Es cruz");
+            $coin.addClass ("isTiles")
+         }
+
+        }, 100);
+
+
+
+    });
+
+});
